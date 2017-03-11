aaebc37e5d9579ff64951e82b78fa885b67d999d
Kirill Konshin <kirill.konshin@gmail.com>
2017-03-10 20:02:20 -0800

    Create React App + SSR Middleware

--- react-scripts/src/router/index.js
+++ react-scripts/src/router/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import {Router, Route} from "react-router";
 import NotFound from '../components/NotFound';
-import App from '../components/App';
 
 export default function(history) {
 
     return <Router history={history}>
-        <Route path='/' component={App}/>
+        <Route path='/' getComponent={() => (new Promise((res) => {
+            require.ensure([], () => {
+                res(require('../components/App').default);
+            })
+        }))}/>
         <Route path='*' component={NotFound}/>
     </Router>;
 
